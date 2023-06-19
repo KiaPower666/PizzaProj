e41c156b3cb4e55ed951175868cab397753513e5
Elena.Krayeva <alenakrayeva@gmail.com>
2023-06-20 00:25:51 +0300

    impemented layout and style(except ingredients)

---
+++ src/ingredients/index.js
@@ -0,0 +1,5 @@
+import styles from "./ingredients.module.scss";
+import { addClassFoundById } from "../helpers/addClassFoundById";
+/* import { addBackground } from '../helpers/addbackground'; */
+
+addClassFoundById("ingredients", styles.ingredients);
