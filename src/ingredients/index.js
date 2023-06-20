1df8ffd0b0e062f56c29845079c16eca1910c27c
Elena.Krayeva <alenakrayeva@gmail.com>
2023-06-20 16:15:54 +0300

    add styles for ingredients

--- src/ingredients/index.js
+++ src/ingredients/index.js
@@ -1,6 +1,10 @@
 import styles from "./ingredients.module.scss";
 import { addClassFoundById } from "../helpers/addClassFoundById";
+import { addClassFoundByClass} from '../helpers/addClassFoundByClass';
 /* import { addBackground } from '../helpers/addbackground'; */
 
 addClassFoundById("ingredients", styles.ingredients);
 addClassFoundById("ingredients", styles.container);
+addClassFoundByClass("input-div", styles.inputdiv);
+addClassFoundByClass("input1", styles.input);
+addClassFoundByClass("checkbox-label", styles.checkboxlabel);
